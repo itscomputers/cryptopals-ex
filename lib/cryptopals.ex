1f5ca0268c5046204bd9378f593662343e092dbb
itscomputers <its_computers@protonmail.com>
2023-05-25 13:40:34 -0600

    set 1 challenges 1 and 2

--- lib/cryptopals.ex
+++ lib/cryptopals.ex
@@ -3,16 +3,37 @@ defmodule Cryptopals do
   Documentation for `Cryptopals`.
   """
 
-  @doc """
-  Hello world.
+  def convert_to_binary(string, base)
+  def convert_to_binary(string, 16), do: Base.decode16!(string, case: :lower)
+  def convert_to_binary(string, 64), do: Base.decode64!(string)
 
-  ## Examples
+  def convert_from_binary(binary, base)
+  def convert_from_binary(binary, 16), do: Base.encode16(binary, case: :lower)
+  def convert_from_binary(binary, 64), do: Base.encode64(binary)
 
-      iex> Cryptopals.hello()
-      :world
+  def convert(string, from, to) do
+    string |> convert_to_binary(from) |> convert_from_binary(to)
+  end
 
-  """
-  def hello do
-    :world
+  def string_to_bytes(string, base) do
+    string
+    |> convert_to_binary(base)
+    |> :binary.bin_to_list()
+  end
+
+  def bytes_to_string(bytes, base) do
+    bytes
+    |> :binary.list_to_bin()
+    |> convert_from_binary(base)
+  end
+
+  def byte_wise_xor(left, right) do
+    Enum.zip(left, right)
+    |> Enum.map(&(Bitwise.bxor(elem(&1, 0), elem(&1, 1))))
+  end
+
+  def xor(left, right, base) do
+    byte_wise_xor(string_to_bytes(left, base), string_to_bytes(right, base))
+    |> bytes_to_string(base)
   end
 end
--- test/cryptopals_test.exs
+++ test/cryptopals_test.exs
@@ -2,7 +2,17 @@ defmodule CryptopalsTest do
   use ExUnit.Case
   doctest Cryptopals
 
-  test "greets the world" do
-    assert Cryptopals.hello() == :world
+  test ".hex_to_base_64" do
+    hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    assert Cryptopals.convert(hex, 16, 64) == expected
+  end
+
+  test ".xor" do
+    hex = "1c0111001f010100061a024b53535009181c"
+    other = "686974207468652062756c6c277320657965"
+    xor = Cryptopals.xor(hex, other, 16)
+    assert xor == "746865206b696420646f6e277420706c6179"
+    [other, xor] |> Enum.map(&(Cryptopals.string_to_bytes(&1, 16))) |> Enum.map(&(IO.inspect(&1)))
   end
 end
